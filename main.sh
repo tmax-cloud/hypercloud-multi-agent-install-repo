88162bd30bb0373091ea7bf96333a0c4174ce47a
tmaxjmc <mincheol_jeon@tmax.co.kr>
2020-11-17 12:37:24 +0900

    fix main.sh for kubectl-bin

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 chmod +x prometheus/prometheus-install.sh
 ./prometheus/prometheus-install.sh
 
-chmod +x kubectl/kubectl.sh
-./kubectl/kubectl.sh
+chmod +x kubectl-bin/kubectl.sh
+./kubectl-bin/kubectl.sh
