4ddd724b704a664b0b35a1c041e06071b76cec2b
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2021-07-08 00:15:20 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,10 @@
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 cd /installer
 
-chmod +x kubectl-bin/kubectl.sh
-./kubectl-bin/kubectl.sh
+chmod +x $SCRIPTDIR/kubectl-bin/kubectl.sh
+$SCRIPTDIR/kubectl-bin/kubectl.sh
 
-chmod +x prometheus/prometheus-install.sh
-cd prometheus && ./prometheus-install.sh
+chmod +x $SCRIPTDIR/prometheus/prometheus-install.sh
+$SCRIPTDIR/prometheus-install.sh
 
