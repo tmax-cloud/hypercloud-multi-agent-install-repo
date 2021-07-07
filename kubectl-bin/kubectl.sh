bd3776c9087cd58000162bba8cd601dc6fc47a21
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2021-07-08 00:16:14 +0900

    Update kubectl.sh

--- kubectl-bin/kubectl.sh
+++ kubectl-bin/kubectl.sh
@@ -1,3 +1,4 @@
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+chmod +x $SCRIPTDIR/kubectl
+mv $SCRIPTDIR/kubectl /usr/local/bin/kubectl
