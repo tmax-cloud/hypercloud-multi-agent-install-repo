d5bd94973ae85380e902e19793a441ca2021065c
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2021-07-08 00:19:00 +0900

    Update prometheus-install.sh

--- prometheus/prometheus-install.sh
+++ prometheus/prometheus-install.sh
@@ -1,4 +1,5 @@
 ## install jq
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o /usr/local/bin/jq
 chmod a+x /usr/local/bin/jq
 
@@ -15,44 +16,44 @@ export PROMETHEUS_ADAPTER_VERSION=v0.5.0
 export ALERTMANAGER_VERSION=v0.20.0
 
 ## set version
-sed -i 's/{ALERTMANAGER_VERSION}/'${ALERTMANAGER_VERSION}'/g' yaml/manifests/alertmanager-alertmanager.yaml
+sed -i 's/{ALERTMANAGER_VERSION}/'${ALERTMANAGER_VERSION}'/g' $SCRIPTDIR/yaml/manifests/alertmanager-alertmanager.yaml
 ##sed -i 's/{GRAFANA_VERSION}/'${GRAFANA_VERSION}'/g' yaml/manifests/grafana-deployment.yaml
-sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' yaml/manifests/kube-state-metrics-deployment.yaml
-sed -i 's/{KUBE_STATE_METRICS_VERSION}/'${KUBE_STATE_METRICS_VERSION}'/g' yaml/manifests/kube-state-metrics-deployment.yaml
-sed -i 's/{NODE_EXPORTER_VERSION}/'${NODE_EXPORTER_VERSION}'/g' yaml/manifests/node-exporter-daemonset.yaml
-sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' yaml/manifests/node-exporter-daemonset.yaml
-sed -i 's/{PROMETHEUS_ADAPTER_VERSION}/'${PROMETHEUS_ADAPTER_VERSION}'/g' yaml/manifests/prometheus-adapter-deployment.yaml
-sed -i 's/{PROMETHEUS_VERSION}/'${PROMETHEUS_VERSION}'/g' yaml/manifests/prometheus-prometheus.yaml
-sed -i 's/{PROMETHEUS_OPERATOR_VERSION}/'${PROMETHEUS_OPERATOR_VERSION}'/g' yaml/setup/prometheus-operator-deployment.yaml
-sed -i 's/{CONFIGMAP_RELOADER_VERSION}/'${CONFIGMAP_RELOADER_VERSION}'/g' yaml/setup/prometheus-operator-deployment.yaml
-sed -i 's/{CONFIGMAP_RELOAD_VERSION}/'${CONFIGMAP_RELOAD_VERSION}'/g' yaml/setup/prometheus-operator-deployment.yaml
+sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' $SCRIPTDIR/yaml/manifests/kube-state-metrics-deployment.yaml
+sed -i 's/{KUBE_STATE_METRICS_VERSION}/'${KUBE_STATE_METRICS_VERSION}'/g' $SCRIPTDIR/yaml/manifests/kube-state-metrics-deployment.yaml
+sed -i 's/{NODE_EXPORTER_VERSION}/'${NODE_EXPORTER_VERSION}'/g' $SCRIPTDIR/yaml/manifests/node-exporter-daemonset.yaml
+sed -i 's/{KUBE_RBAC_PROXY_VERSION}/'${KUBE_RBAC_PROXY_VERSION}'/g' $SCRIPTDIR/yaml/manifests/node-exporter-daemonset.yaml
+sed -i 's/{PROMETHEUS_ADAPTER_VERSION}/'${PROMETHEUS_ADAPTER_VERSION}'/g' $SCRIPTDIR/yaml/manifests/prometheus-adapter-deployment.yaml
+sed -i 's/{PROMETHEUS_VERSION}/'${PROMETHEUS_VERSION}'/g' $SCRIPTDIR/yaml/manifests/prometheus-prometheus.yaml
+sed -i 's/{PROMETHEUS_OPERATOR_VERSION}/'${PROMETHEUS_OPERATOR_VERSION}'/g' $SCRIPTDIR/yaml/setup/prometheus-operator-deployment.yaml
+sed -i 's/{CONFIGMAP_RELOADER_VERSION}/'${CONFIGMAP_RELOADER_VERSION}'/g' $SCRIPTDIR/yaml/setup/prometheus-operator-deployment.yaml
+sed -i 's/{CONFIGMAP_RELOAD_VERSION}/'${CONFIGMAP_RELOAD_VERSION}'/g' $SCRIPTDIR/yaml/setup/prometheus-operator-deployment.yaml
 
 ## step1: apply setup
-kubectl create -f yaml/setup/
+kubectl create -f $SCRIPTDIR/yaml/setup/
 sleep 30s
 
 ## step2: apply manifests
-kubectl create -f yaml/manifests/
+kubectl create -f $SCRIPTDIR/yaml/manifests/
 ##kubectl get svc -n monitoring prometheus-k8s -o yaml | sed "s|type: NodePort|type: LoadBalancer|g" | kubectl replace -f -
 ##kubectl get svc -n monitoring grafana -o yaml | sed "s|type: ClusterIP|type: LoadBalancer|g" | kubectl replace -f -
 
-kubectl get prometheus k8s -n monitoring -o json | jq 'del(.spec.storage)' > prometheus-k8s.json
+kubectl get prometheus k8s -n monitoring -o json | jq 'del(.spec.storage)' > $SCRIPTDIR/prometheus-k8s.json
 kubectl delete prometheus k8s -n monitoring
 kubectl apply -f prometheus-k8s.json
 
-rm prometheus-k8s.json -f
+rm $SCRIPTDIR/prometheus-k8s.json -f
 
 ## step3: apply discovery
-kubectl create -f yaml/kube-controller-manager-prometheus-discovery.yaml
-kubectl create -f yaml/kube-scheduler-prometheus-discovery.yaml
+kubectl create -f $SCRIPTDIR/yaml/kube-controller-manager-prometheus-discovery.yaml
+kubectl create -f $SCRIPTDIR/yaml/kube-scheduler-prometheus-discovery.yaml
 
-kubectl get servicemonitor -n monitoring kube-controller-manager -o json | jq 'del(.spec.endpoints[].metricRelabelings)' > kube-controller-manager.json
-kubectl get servicemonitor -n monitoring kube-scheduler -o json | jq 'del(.spec.endpoints[].metricRelabelings)' > kube-scheduler.json
+kubectl get servicemonitor -n monitoring kube-controller-manager -o json | jq 'del(.spec.endpoints[].metricRelabelings)' > $SCRIPTDIR/kube-controller-manager.json
+kubectl get servicemonitor -n monitoring kube-scheduler -o json | jq 'del(.spec.endpoints[].metricRelabelings)' > $SCRIPTDIR/kube-scheduler.json
 
-kubectl apply -f kube-controller-manager.json
-kubectl apply -f kube-scheduler.json
+kubectl apply -f $SCRIPTDIR/kube-controller-manager.json
+kubectl apply -f $SCRIPTDIR/kube-scheduler.json
 
-rm kube-controller-manager.json  -f
+rm $SCRIPTDIR/kube-controller-manager.json  -f
 
 ## step4: add labels
 pods=($(kubectl get pod -n kube-system  | grep kube-scheduler | awk '{print$1}'))
