b555a706ba3a95eb9960256fca0195d6e6e32748
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2021-07-08 00:23:26 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,5 +6,5 @@ chmod +x $SCRIPTDIR/kubectl-bin/kubectl.sh
 $SCRIPTDIR/kubectl-bin/kubectl.sh
 
 chmod +x $SCRIPTDIR/prometheus/prometheus-install.sh
-$SCRIPTDIR/prometheus-install.sh
+$SCRIPTDIR/prometheus/prometheus-install.sh
 
